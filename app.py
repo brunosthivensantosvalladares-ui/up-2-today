21b4c11fe74c89a56207f4012663fc3a899ae478
ted-gestao <bruno.sthivensantosvalladares@gmail.com>
2026-02-03 22:28:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from fpdf import FPDF
 # --- CONFIGURAÇÕES DE MARCA ---
 NOME_SISTEMA = "Ted"
 SLOGAN = "Seu Controle. Nossa Prioridade."
-LOGO_URL = "https://i.postimg.cc/wTbmmT7r/logo-png.png"
+LOGO_URL = "https://i.postimg.cc/9MHLwp26/logo-png.png"
 ORDEM_AREAS = ["Motorista", "Borracharia", "Mecânica", "Elétrica", "Chapeamento", "Limpeza"]
 LISTA_TURNOS = ["Não definido", "Dia", "Noite"]
 COR_AZUL, COR_VERDE = "#3282b8", "#8ac926"
