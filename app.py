c7cf374f0d0e3a9bd78b64c34e74d95af6a366fd
ted-gestao <bruno.sthivensantosvalladares@gmail.com>
2026-02-04 00:23:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from fpdf import FPDF
 # --- CONFIGURAÇÕES DE MARCA ---
 NOME_SISTEMA = "Up 2 Today"
 SLOGAN = "Seu Controle. Nossa Prioridade."
-LOGO_URL = "https://i.postimg.cc/9MHLwp26/logo-png.png"
+LOGO_URL = "https://i.postimg.cc/130qmhM1/logo.png"
 ORDEM_AREAS = ["Motorista", "Borracharia", "Mecânica", "Elétrica", "Chapeamento", "Limpeza"]
 LISTA_TURNOS = ["Não definido", "Dia", "Noite"]
 
